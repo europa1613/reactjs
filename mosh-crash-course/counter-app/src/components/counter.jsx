425545aa5cad2de49c1eb7f9ad20be788c5d0a91
Arvind <europa1613@users.noreply.github.com>
2020-03-12 23:45:06 -0500

    mosh crash course react

---
+++ mosh-crash-course/counter-app/src/components/counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  hanldeIncrement() {
+      console.log('Incrementing!')
+  }
+  render() {
+    let classes = this.getCountClass();
+    return (
+      <div>
+        <span className={classes}>{this.formatCount()}</span>
+        <button onClick={this.hanldeIncrement} className="btn btn-secondary  btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getCountClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
