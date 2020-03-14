ae27f718a6bc34eaccea10069897b27dc0eee94b
Arvind <europa1613@users.noreply.github.com>
2020-03-13 23:02:16 -0500

    Reset doesnt work due to a internal state is available for counter

--- mosh-crash-course/counter-app/src/components/counters.jsx
+++ mosh-crash-course/counter-app/src/components/counters.jsx
@@ -13,13 +13,29 @@ class Counters extends Component {
 
     handleDelete = counterId => {
         console.log("Handling Delete!", counterId);
-        const counters = this.state.counters.filter(counter => counter.id !== counterId);
-        this.setState({counters});
+        const counters = this.state.counters.filter(
+            counter => counter.id !== counterId
+        );
+        this.setState({ counters });
+    };
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
     };
 
     render() {
         return (
             <div>
+                <button
+                    className="btn btn-primary btn-sm m-2"
+                    onClick={this.handleReset}
+                >
+                    Reset
+                </button>
                 {this.state.counters.map(counter => (
                     <Counter
                         key={counter.id}
