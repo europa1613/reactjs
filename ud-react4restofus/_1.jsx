2c01c8a57b520a1f6620d31b43471afc85e83658
Arvind <europa1613@users.noreply.github.com>
2020-05-12 22:26:43 -0500

    ud-react4restofus: codepen link

--- ud-react4restofus/_1.jsx
+++ ud-react4restofus/_1.jsx
@@ -1,11 +1,15 @@
+/**
+ *https://codepen.io/arivndrukmaji/pen/eYpjBBR?editors=1111
+ */
+
 function OurApp() {
-  return (
-    <div>
-      <h1 className="special">Our Amazing App Header</h1>
-      <p>The current time is {new Date().toLocaleString()}</p>
-      <small>Copyright text here.</small>
-     </div>
-  )
+    return ( <
+        >
+        <h1 className="special">Our Amazing App Header</h1> <
+        p > The current time is { new Date().toLocaleString() } < /p> <
+        small > Copyright text here. < /small> <
+        />
+    )
 }
 
 setInterval(() => ReactDOM.render(<OurApp/>, document.querySelector('#app')), 1000)
