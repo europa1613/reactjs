2cf67882df39d5e528cfda3e5c16284dae3195e5
Arvind <europa1613@users.noreply.github.com>
2025-07-06 19:59:18 -0500

    state

---
+++ quick-react/material/D4_State_useState/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function Counter({ label, children }) {
+    const [count, setCount] = useState(0);
+
+    console.log("Executing Counter " + label);
+
+    return (
+        <article className="card">
+            <h2>{label}</h2>
+            <button onClick={() => setCount(count + 1)}>
+                count is {count}
+            </button>
+            { children }
+        </article>
+    )
+}
+
+export default Counter;
