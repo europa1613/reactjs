b19aa7369837a80c0559aa6f232186afff696ebb
arvns-astrom4pro <europa1613@users.noreply.github.com>
2025-10-20 19:19:12 -0500

    Initial MoviesGrid Component

---
+++ reactjs-thefastway/moviedux/src/components/MoviesGrid.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+
+export default function MoviesGrid() {
+  return (
+    <div className="movies-grid">{/* Movie items will be rendered here */}</div>
+  );
+}
